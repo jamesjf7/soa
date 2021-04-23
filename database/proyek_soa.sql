c5f3bfe0bcfab11ece007ecdc68a7dd545701d24
jamesjf7 <jamesjeremyf2000@yahoo.com>
2021-04-23 17:36:37 +0800

    Database

--- database/proyek_soa.sql
+++ database/proyek_soa.sql
@@ -1,6 +1,6 @@
 /*
-SQLyog Enterprise v13.1.1 (64 bit)
-MySQL - 10.1.37-MariaDB : Database - proyek_soa
+SQLyog Ultimate v13.1.1 (64 bit)
+MySQL - 10.4.17-MariaDB : Database - proyek_soa
 *********************************************************************
 */
 
@@ -14,58 +14,59 @@ MySQL - 10.1.37-MariaDB : Database - proyek_soa
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`proyek_soa` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-/*Table structure for table `favorite` */
+USE `proyek_soa`;
 
-DROP TABLE IF EXISTS `favorite`;
+/*Table structure for table `plans` */
 
-CREATE TABLE `favorite` (
-  `user_id` int(11) NOT NULL,
-  `resep_id` int(11) NOT NULL,
-  PRIMARY KEY (`user_id`,`resep_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `plans`;
 
-/*Data for the table `favorite` */
-
-/*Table structure for table `paket` */
-
-DROP TABLE IF EXISTS `paket`;
-
-CREATE TABLE `paket` (
+CREATE TABLE `plans` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `nama` varchar(255) DEFAULT NULL,
-  `harga` int(11) DEFAULT NULL,
-  `durasi` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-/*Data for the table `paket` */
+/*Data for the table `plans` */
 
-insert  into `paket`(`id`,`nama`,`harga`,`durasi`) values 
+insert  into `plans`(`id`,`name`,`price`,`duration`) values 
 (1,'SILVER',100000,30),
 (2,'GOLD',150000,60),
 (3,'DIAMOND',200000,90);
 
-/*Table structure for table `user` */
+/*Table structure for table `transactions` */
 
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `transactions`;
 
-CREATE TABLE `user` (
+CREATE TABLE `transactions` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `nama` varchar(255) DEFAULT NULL,
-  `umur` int(11) DEFAULT NULL,
-  `berat` int(11) DEFAULT NULL COMMENT 'kg',
-  `fat` int(3) DEFAULT NULL COMMENT '0 s/d 100',
-  `role` int(1) DEFAULT NULL COMMENT '0 = admin, 1 = user',
-  `saldo` int(11) DEFAULT NULL,
-  `is_premium` int(1) DEFAULT NULL COMMENT '0 = tidak, 1 = ya',
-  `is_active` int(1) DEFAULT '1' COMMENT '0 = tidak aktif, 1 = aktif',
-  `api_hit` int(11) DEFAULT '0',
-  `paket_id` int(11) DEFAULT NULL,
-  `paket_start` timestamp NULL DEFAULT NULL,
+  `plan_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `user` */
+/*Data for the table `transactions` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `weight` int(11) DEFAULT NULL COMMENT 'kg',
+  `fat` int(3) DEFAULT NULL COMMENT '0 s/d 100',
+  `role` int(1) DEFAULT NULL COMMENT '0 = admin, 1 = user',
+  `balance` int(11) DEFAULT NULL,
+  `is_active` int(1) DEFAULT 1 COMMENT '0 = tidak aktif, 1 = aktif',
+  `api_hit` int(11) DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
