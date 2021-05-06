1054eb4d2f4f37e5ad6215892d66058d63df3d29
jamesjf7 <jamesjeremyf2000@yahoo.com>
2021-05-06 13:44:37 +0800

    logs path

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(
         {
             stream: fs.createWriteStream(
                 // `./logs/${moment().format("Y.MM.D")}.log`,
-                `.logs/access.log`,
+                `./logs/access.log`,
                 {
                     flags: "a",
                 }
