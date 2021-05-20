2071d3d8a3a08787b366c9b0b3ac33f3117d619b
jamesjf7 <jamesjeremyf2000@yahoo.com>
2021-05-20 13:13:41 +0800

    routes

--- database/proyek_soa.sql
+++ database/proyek_soa.sql
@@ -78,7 +78,7 @@ CREATE TABLE `users` (
 
 insert  into `users`(`id`,`name`,`email`,`username`,`password`,`token`,`image`,`age`,`role`,`balance`,`is_active`,`api_hit`,`last_hit`) values 
 (1,'admin','admin1@gmail.com','admin1','admin1','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJhZG1pbjEiLCJyb2xlIjoiMCIsImlhdCI6MTYyMDgzMTM1M30.30FHfJaLp6mYmv21le_RDaZR1dHbS81VrWbG-n2K6gc',NULL,22,0,0,1,100,NULL),
-(2,'admin','user1@gmail.com','user1','user1','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJ1c2VyMSIsInJvbGUiOiIxIiwiaWF0IjoxNjIwODMxMzc5fQ.0BdUhYCwRWkCadKP01jSrPwJVqBcFk6Sjee1a238UCQ',NULL,22,1,0,1,100,NULL);
+(2,'admin','user1@gmail.com','user1','user1','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJ1c2VyMSIsInJvbGUiOiIxIiwiaWF0IjoxNjIwODMxMzc5fQ.0BdUhYCwRWkCadKP01jSrPwJVqBcFk6Sjee1a238UCQ',NULL,22,1,0,1,1000000000,NULL);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
