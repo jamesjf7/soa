779334992df1aaef98ae15801119fb76e0ebefc6
jamesjf7 <jamesjeremyf2000@yahoo.com>
2021-05-29 00:05:54 +0800

    config

---
+++ database/config.js
@@ -0,0 +1,21 @@
+const local = {
+    database: {
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "proyek_soa",
+    },
+};
+
+const production = {
+    database: {
+        host: "remotemysql.com",
+        user: "iVp5wJZ4S2",
+        password: "TcuZjcJjrD",
+        database: "iVp5wJZ4S2",
+        port: 3306,
+    },
+};
+
+module.exports = production;
+// module.exports = production;
